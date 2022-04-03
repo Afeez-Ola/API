df6d31e2bc779668d71b83f612240b97c808e4eb
Afeez Bolaji <afeezbolajiola@gmail.com>
2022-04-03 23:09:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ mongoose.connect('mongodb://localhost/BOOK-STORE', { useUnifiedTopology: true })
 mongoose.connection.on('error', err => console.error(err));
 mongoose.connection.on('connected', () => console.log("connected!"));
 
-Genre = require('./models/genre.js')
-Book = require('./models/book.js')
+ Genre = require('./models/genre.js')
+ Book = require('./models/book.js')
 
 app.get('/', (req, res) => {
     res.send('hello world!');
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 app.get('/api/genres', async(req, res) => {
 
     try {
-        let genres = await Genre.find();
+        const genres = await Genre.find();
         res.json(genres);
     } catch (err) {
 
@@ -32,7 +32,7 @@ app.get('/api/genres', async(req, res) => {
 
 app.get('/api/books', async(req, res) => {
     try {
-        let books = await Book.find()
+        const books = await Book.find()
         res.json(books)
     } catch (err) {
         console.log(err)
